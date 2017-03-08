4b9b244ea77e2310667061210aca465a5c985648
Matheus <matheus@Brenners-Mac.local>
2017-03-08 10:11:00 -0300

    part 2

---
+++ Watch Extension/FlightRowController.swift
@@ -0,0 +1,36 @@
+//
+//  FlightRowController.swift
+//  AirAber
+//
+//  Created by Matheus on 08/03/17.
+//  Copyright © 2017 Mic Pringle. All rights reserved.
+//
+
+import WatchKit
+
+class FlightRowController: NSObject {
+
+  @IBOutlet var separator: WKInterfaceSeparator!
+  @IBOutlet var originLabel: WKInterfaceLabel!
+  @IBOutlet var destinationLabel: WKInterfaceLabel!
+  @IBOutlet var flightNumberLabel: WKInterfaceLabel!
+  @IBOutlet var statusLabel: WKInterfaceLabel!
+  @IBOutlet var planeImage: WKInterfaceImage!
+  
+  var flight: Flight? {
+    didSet {
+      guard let flight = flight else { return }
+      originLabel.setText(flight.origin)
+      destinationLabel.setText(flight.destination)
+      flightNumberLabel.setText(flight.number)
+      
+      if flight.onSchedule {
+        statusLabel.setText("On Time")
+      } else {
+        statusLabel.setText("Delayed")
+        statusLabel.setTextColor(.red)
+      }
+    }
+  }
+  
+}
---
+++ Watch Extension/ScheduleInterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  ScheduleInterfaceController.swift
+//  AirAber
+//
+//  Created by Matheus on 08/03/17.
+//  Copyright © 2017 Mic Pringle. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class ScheduleInterfaceController: WKInterfaceController {
+
+    @IBOutlet var flightsTable: WKInterfaceTable!
+ 
+  var flights = Flight.allFlights()
+  
+  override func awake(withContext context: Any?) {
+    super.awake(withContext: context)
+    flightsTable.setNumberOfRows(flights.count, withRowType: "FlightRow")
+    
+    for index in 0..<flightsTable.numberOfRows {
+      guard let controller = flightsTable.rowController(at: index) as? FlightRowController else { continue }
+      
+      controller.flight = flights[index]
+    }
+  }
+
+}
+
